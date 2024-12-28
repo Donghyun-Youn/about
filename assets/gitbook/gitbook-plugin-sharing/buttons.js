f8c72e1d578445929e61f49a7e72140aaec459e6
Donghyun-Youn <dhyoun93@gmail.com>
2024-12-28 20:12:10 +0900

    Update buttons.js

--- assets/gitbook/gitbook-plugin-sharing/buttons.js
+++ assets/gitbook/gitbook-plugin-sharing/buttons.js
@@ -10,7 +10,7 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
         },
         'twitter': {
             'label': 'Twitter',
-            'icon': 'fa fa-square-twitter',
+            'icon': 'fa fa-twitter-square',
             'onClick': function(e) {
                 e.preventDefault();
                 window.open('http://twitter.com/home?status='+encodeURIComponent(document.title+' '+location.href));
